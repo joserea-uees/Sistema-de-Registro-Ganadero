373034bbebec71622afb955ced6c4cdf50f51b7c
Jose Rea <jose.rea@uees.edu.ec>
2026-01-30 14:03:04 -0500

    [FEAT] - Visualizacion de campos a Ingresar

--- Sistema de Registro/Login.vb
+++ Sistema de Registro/Login.vb
@@ -2,27 +2,21 @@
 Public Class Login
     'Conexion a la base de datos SQL Server
     Dim conexionSQL As SqlConnection
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        conexionSQL = New SqlConnection("server=MSI; database=h_san_jose; integrated security=true")
-        Try
-            conexionSQL.Open()
-            'MessageBox.Show("Conexión exitosa a la base de datos", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        Catch ex As Exception
-            MessageBox.Show("Error al conectar a la base de datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        Finally
-            conexionSQL.Close()
-        End Try
-    End Sub
 
     Private Sub User_Click(sender As Object, e As EventArgs) Handles User.Click
         User.Text = ""
 
     End Sub
-
     Private Sub password_Click(sender As Object, e As EventArgs) Handles password.Click
         password.Text = ""
     End Sub
+    Private Sub User_LostFocus(sender As Object, e As EventArgs) Handles User.LostFocus
+        If String.IsNullOrWhiteSpace(User.Text) Then User.Text = "Usuario"
+    End Sub
 
+    Private Sub password_LostFocus(sender As Object, e As EventArgs) Handles password.LostFocus
+        If String.IsNullOrWhiteSpace(password.Text) Then password.Text = "Contraseña"
+    End Sub
 
 
     'Evento del botón de inicio de sesión
@@ -34,7 +28,7 @@ Public Class Login
 
         Dim usuario As String = User.Text.Trim() '.Trim() elimina espacios en blanco al inicio y al final
         Dim contrasena As String = password.Text.Trim()
-        Dim conexionString As String = "server=MSI; database=h_san_jose; integrated security=true"
+        Dim conexionString As String = "server=MSI; database=h_san_jose; integrated security=true" 'Cadena de conexión a la base de datos
 
         Using conexion As New SqlConnection(conexionString)
             Try
@@ -47,8 +41,8 @@ Public Class Login
 
                     If resultado > 0 Then
                         Dim Menu_Principal As New Menu_Principal()
-                        Menu_Principal.Show()
-                        Me.Hide()
+                        Menu_Principal.Show() 'Muestra el formulario del menú principal
+                        Me.Hide() 'Oculta el formulario de inicio de sesión
                     Else
                         MessageBox.Show("Usuario o contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
                     End If
